77190f3838be4c1162fd2ccdc2771f54c091fedc
Josh Larabie <Josh.Larabie@gmail.com>
2022-07-16 14:56:23 -0400

    Add home, contact, and about pages

--- blog/app.js
+++ blog/app.js
@@ -18,6 +18,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+	res.render("home", { homeStartingContent: homeStartingContent });
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+	res.render("contact", { contactContent: contactContent });
+});
+
 app.listen(3000, function () {
 	console.log("Server started on port 3000");
 });
