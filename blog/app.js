bae30bb64bd673a129d090fab3da0b65d08f6205
Josh Larabie <Josh.Larabie@gmail.com>
2022-07-28 11:37:12 -0400

    Implement base blog display functionality

--- blog/app.js
+++ blog/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
 	"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,7 +14,7 @@ const contactContent =
 
 const app = express();
 
-const posts = [];
+let posts = [];
 
 app.set("view engine", "ejs");
 
@@ -21,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-	res.render("home", { homeStartingContent: homeStartingContent });
+	res.render("home", { homeStartingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", (req, res) => {
@@ -41,9 +42,27 @@ app.post("/compose", (req, res) => {
 		postTitle: req.body.postTitle,
 		postBody: req.body.postBody,
 	};
-	
+
 	posts.push(post);
-	console.log(posts);
+	res.redirect("/");
+});
+
+app.get("/post/:postTitle", (req, res) => {
+	const requestedTitle = req.params.postTitle
+
+	posts.forEach((post) => {
+		if (post.postTitle === requestedTitle) {
+			res.render("post", {
+				postTitle: post.postTitle,
+				postBody: post.postBody,
+			});
+		} else {
+			res.render("post", {
+				postTitle: requestedTitle,
+				postBody: "Post not found",
+			});
+		}
+	});
 });
 
 app.listen(3000, function () {
