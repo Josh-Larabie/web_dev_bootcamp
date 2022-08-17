12d71e0e6a445ca250130832021754674f51fca8
Josh Larabie <Josh.Larabie@gmail.com>
2022-08-16 22:18:55 -0400

    Add mongoose (MongoDB) integration

--- blog-upgraded/app.js
+++ blog-upgraded/app.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
 
 const homeStartingContent =
 	"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,28 +13,54 @@ const contactContent =
 
 const app = express();
 
-let insertPost = function (data) {
-  
-	const client = new MongoClient(process.env.DB_URI);
-	const database = client.db(process.env.DB_NAME);
-	const posts = database.collection(process.env.DB_COLLECTION);
-	const query = data;
-	const post = posts.insertOne(query);
-	console.log("Inserted: " + data);
-	// Ensures that the client will close when you finish/error
-	client.close();
-};
-
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+
+const postSchema = {
+	title: String,
+	content: String,
+};
+
+const Post = mongoose.model(process.env.DB_COLLECTION, postSchema);
 
 app.get("/", function (req, res) {
-	res.render("home", {
-		startingContent: homeStartingContent,
-		posts: posts,
+	Post.find({}, function (err, posts) {
+		res.render("home", {
+			startingContent: homeStartingContent,
+			posts: posts,
+		});
+	});
+});
+
+app.get("/compose", function (req, res) {
+	res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+	const post = new Post({
+		title: req.body.postTitle,
+		content: req.body.postBody,
+	});
+
+	post.save(function (err) {
+		if (!err) {
+			res.redirect("/");
+		}
+	});
+});
+
+app.get("/posts/:postId", function (req, res) {
+	const requestedPostId = req.params.postId;
+
+	Post.findOne({ _id: requestedPostId }, function (err, post) {
+		res.render("post", {
+			title: post.title,
+			content: post.content,
+		});
 	});
 });
 
@@ -45,38 +70,6 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
 	res.render("contact", { contactContent: contactContent });
-});
-
-app.get("/compose", function (req, res) {
-	res.render("compose");
-});
-
-app.post("/compose", function (req, res) {
-	const post = {
-		title: req.body.postTitle,
-		content: req.body.postBody,
-	};
-
-	insertPost(post); // insert post into database
-	console.log("Inserted into DB.");
-	posts.push(post);
-
-	res.redirect("/");
-});
-
-app.get("/posts/:postName", function (req, res) {
-	const requestedTitle = _.lowerCase(req.params.postName);
-
-	posts.forEach(function (post) {
-		const storedTitle = _.lowerCase(post.title);
-
-		if (storedTitle === requestedTitle) {
-			res.render("post", {
-				title: post.title,
-				content: post.content,
-			});
-		}
-	});
 });
 
 app.listen(3000, function () {
