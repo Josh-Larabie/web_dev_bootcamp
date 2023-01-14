417c94a40bed1b8fd584f16dd44435754df507f5
Josh Larabie <josh.larabie@gmail.com>
2023-01-14 22:32:08 +0000

    Modify App render to include Notes component mapping.

--- react-keeper/src/components/App.jsx
+++ react-keeper/src/components/App.jsx
@@ -1,12 +1,21 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Note />
+			{notes.map((noteItem) => {
+				return (
+					<Note
+						key={noteItem.key}
+						title={noteItem.title}
+						content={noteItem.content}
+					/>
+				);
+			})}
 			<Footer />
 		</div>
 	);
